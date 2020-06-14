f032f1aaceefcd32f2e71f93ec60a17c8e77ee30
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-06-14 17:52:20 +0530

    add signup feature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+app.use(express.urlencoded({extended:true}));
 app.use(express.static("css"));
 app.use(cors());
 
--- handlers/auth.js
+++ handlers/auth.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const app = express.Router();
+const Cloudant = require('@cloudant/cloudant');
+const config = require(`../config/all.${process.env.NODE_ENV}.json`);
+const cloudantConfig = config.ibm_services;
+
+const cloudant = new Cloudant(
+    { 
+        url: cloudantConfig.url, 
+        plugins: { 
+            iamauth: { 
+                iamApiKey: cloudantConfig.iamKey 
+            }
+        } 
+    }
+);
+
+const db = cloudant.db.use("ibm_hackchallenge");
 
 app.get("/auth/login",(req,res)=>{
     res.render('login.ejs');
@@ -7,6 +23,16 @@ app.get("/auth/login",(req,res)=>{
 
 app.get("/auth/signup",(req,res)=>{
     res.render('signup.ejs');
+})
+
+app.post("/auth/signup",(req,res)=>{
+    const {uname,email,psw} = req.body; 
+    db.insert({_id:email,username:uname,email,password:psw[0]},(err,data)=>{
+        if(!err)
+            res.send("successfully signed you up");
+        else
+            res.send("soemthig went wrong");
+    })
 })
 
 
