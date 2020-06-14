1784b5603d5218a24ce8a805107dc90d9651b472
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-06-14 14:21:17 +0530

    add primary backend schema

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.static("css"));
+app.use(cors());
+
+const auth = require("./handlers/auth");
+
+app.use(auth);
+app.listen(3030,()=>{
+    console.log("Server is up");
+})
---
+++ handlers/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express.Router();
+
+app.get("/auth/login",(req,res)=>{
+    // for login
+})
+
+app.get("/auth/signup",(req,res)=>{
+    // for signup
+})
+
+
+
+module.exports = app;
