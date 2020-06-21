d9e6577d4b2e19cfd1905df209cea717603a8f5c
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-06-22 00:48:08 +0530

    add common file for all routes

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const cors = require("cors");
 const app = express();
 const session = require("express-session");
 
-app.use(express.urlencoded({extended:true}));
-app.use(session({ secret: "cats",resave:false, saveUninitialized: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(express.static("css"));
 app.use(cors());
 
 const auth = require("./handlers/auth");
+const routes = require("./handlers/routes");
 
+app.use(routes);
 app.use(auth);
 
-app.listen(3030,()=>{
-    console.log("Server is up");
-})
+app.listen(3030, () => {
+  console.log("Server is up");
+});
--- handlers/auth.js
+++ handlers/auth.js
@@ -1,63 +1,65 @@
 const express = require("express");
 const app = express.Router();
-const Cloudant = require('@cloudant/cloudant');
+const Cloudant = require("@cloudant/cloudant");
 const config = require(`../config/all.${process.env.NODE_ENV}.json`);
-const cloudantConfig = config['ibm_services_'+process.env.user];
-const cloudant = new Cloudant({url: cloudantConfig.url, plugins: {iamauth: {iamApiKey: cloudantConfig.iamKey}}});
+const cloudantConfig = config["ibm_services_" + process.env.user];
+const cloudant = new Cloudant({
+  url: cloudantConfig.url,
+  plugins: { iamauth: { iamApiKey: cloudantConfig.iamKey } },
+});
 const db = cloudant.db.use("ibm_hackchallenge");
 
-app.get("/auth/login",(req,res)=>{
-    res.render('login.ejs');
-})
-
-app.get("/auth/signup",(req,res)=>{
-    res.render('signup.ejs');
-})
-
-
-app.post("/auth/signup",(req,res)=>{
-    const {uname,email,psw} = req.body;
-    db.insert({_id:email,username:uname,email,password:psw[0]},(err,data)=>{
-        if(!err){
-            res.send("successfully signed you up");
-            console.log(data);
-        }
-        else {
-            res.send("soemthig went wrong");
-            console.log(err);
-        }
-    })
-})
-
-app.post("/auth/login",(req,res)=>{
-    const {uname,psw} = req.body;
-    
-    db.get(uname, (err, data)=>{
-        if(!err){
-            const {password} =data;
-            
-            if (password === psw) {
-                req.session.user = uname;
-                res.send("Successfuly logged in");
-            }
-            else {
-                res.send("Wrong password");
-            }
-        }
-        else {
-            res.send("soemthig went wrong");
-            console.log(err);
-        }
-    });
+app.get("/auth/login", (req, res) => {
+  res.render("login.ejs");
 });
 
-app.get("/auth/logout",(req,res)=>{
-    req.session.destroy();
-    res.send("logout success");
-})
+app.get("/auth/signup", (req, res) => {
+  res.render("signup.ejs");
+});
 
-app.get("/",(req,res)=>{
-    res.render('home.ejs',{user:req.session.uname});
-})
+app.post("/auth/signup", (req, res) => {
+  const { uname, email, psw } = req.body;
+  db.insert(
+    { _id: email, username: uname, email, password: psw[0] },
+    (err, data) => {
+      if (!err) {
+        res.send("successfully signed you up");
+        console.log(data);
+      } else {
+        res.send("soemthig went wrong");
+        console.log(err);
+      }
+    }
+  );
+});
+
+app.post("/auth/login", (req, res) => {
+  const { uname, psw } = req.body;
+
+  db.get(uname, (err, data) => {
+    if (!err) {
+      const { password } = data;
+
+      if (password === psw) {
+        req.session.user = uname;
+        res.send("Successfuly logged in");
+      } else {
+        res.send("Wrong password");
+      }
+    } else {
+      res.send("soemthig went wrong");
+      console.log(err);
+    }
+  });
+});
+
+app.get("/auth/logout", (req, res) => {
+  req.session.destroy();
+  res.send("logout success");
+});
+
+app.get("/", (req, res) => {
+  res.render("home.ejs", { user: req.session.user });
+});
 
 module.exports = app;
