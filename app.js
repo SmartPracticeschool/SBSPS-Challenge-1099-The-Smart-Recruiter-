76f4a176a77832c3d9fec50510c94c64940a2633
AirndamSharma <arindamsharma1998@gmail.com>
2020-06-23 21:50:51 +0530

    added authintation with login/signup modal

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const session = require("express-session");
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(express.static("css"));
+app.use(express.static("script"));
 app.use(cors());
 
-// const auth = require("./handlers/auth");
-const auth = require("./handlers/auth2.0");
-// const routes = require("./handlers/routes");
+const auth = require("./handlers/auth");
+const routes = require("./handlers/routes");
 
 app.use(routes);
 app.use(auth);
 
 app.listen(3030, () => {
-  console.log("Server is up");
+  console.log("Server is up :- http://localhost:3030");
 });
--- handlers/routes.js
+++ handlers/routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express.Router();
 
 app.get("/", (req, res) => {
-  res.render("home.ejs", { user: req.session.user });
+  // console.log(req.session);
+  res.render("home.ejs", { user:req.session.user ,status:false ,flag:0});
 });
 
 module.exports = app;
