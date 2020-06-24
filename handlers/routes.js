a820d0ad637be47492467adbccb361ef690b2c25
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-06-24 15:04:11 +0530

    make simple c++ editor

--- handlers/routes.js
+++ handlers/routes.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express.Router();
 
 app.get("/", (req, res) => {
-  // console.log(req.session);
   res.render("home.ejs", { user:req.session.user ,status:false ,flag:0});
 });
+
 app.get("/profile", (req, res) => {
-  // console.log(req.session);
   res.render("profile.ejs", { user:req.session.user});
 });
+
+app.get("/app/editor",(req,res)=>{
+  res.render("editor.ejs");
+})
 
 module.exports = app;
